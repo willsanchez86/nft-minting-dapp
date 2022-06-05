12780cd81af752bb813f6d92db02016ddcd3df1f
Will <williamlancelotsanchez@gmail.com>
2022-06-04 21:03:38 -0400

    vipMint functioning properly

--- src/App.js
+++ src/App.js
@@ -283,6 +283,19 @@ function App() {
             </div>
           </main>
 
+          {/* Status */}
+          {status && (
+            <div
+              className={`border ${
+                status.success ? 'border-green-500' : 'border-brand-pink-400 '
+              } rounded-md text-start h-full px-4 py-4 w-full mx-auto mt-8 md:mt-4"`}
+            >
+              <p className="flex flex-col space-y-2 text-white text-sm md:text-base break-words ...">
+                {status.message}
+              </p>
+            </div>
+          )}
+
           <footer className="h-1/5 p-3 border-t border-gray-300">
             <h1 className="font-coiny text-2xl font-bold p-4">
               CONTRACT ADDRESS
